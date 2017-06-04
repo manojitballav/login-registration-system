8572d1c577e46c2e5cc19fc3f90f7d6ea4ce5cbe
manojitballav <manojitocto@gmail.com>
2017-06-04 18:28:57 +0530

    Final commit

---
+++ home.php
@@ -0,0 +1,64 @@
+<!-- PHP code starts here -->
+
+<?php
+session_start();
+include_once 'dbconnect.php';
+
+if (!isset($_SESSION['userSession'])) {
+	header("Location: index.php");
+}
+
+$query = $DBcon->query("SELECT * FROM tbl_users WHERE user_id=".$_SESSION['userSession']);
+$userRow=$query->fetch_array();
+$DBcon->close();
+
+?>
+<!-- End of php code -->
+
+<!-- HTML code starts from here -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Holla - <?php echo $userRow['email']; ?></title>
+
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
+
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Books</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home </a></li>
+            <li><a href="#">Upload</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp; <?php echo $userRow['username']; ?></a></li>
+            <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp; Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+<!-- The main body starts here -->
+<div class="container" style="margin-top:150px; text-align:center; font-family:Verdana, sans-serif;">
+	<!-- <a href="#">Welcome to the new submission platform</a><br /><br /> -->
+    <h1>Welcome to BOOKS !</h1></br></br>
+    <p style="font-size: 20px;">Get programming books at one place</p>
+</div>
+
+</body>
+</html>
